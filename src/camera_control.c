1bd6749b3599b6f9953e9d75244077d044c29b72
renehorstmann <r.horsty@gmail.com>
2021-02-02 14:14:02 +0100

    move ...

--- src/camera_control.c
+++ src/camera_control.c
@@ -8,27 +8,36 @@ static struct {
     vec2 pos;
     float size;
 
+    vec2 pos0;
+
+    vec2 move0;
+
+#if GLES
+    float size0;
     vec2 touch[2];
     bvec2 touching;
-
     float distance0;
-    float size0;
-    vec2 move0;
+#else
+    vec2 pointer_pos;
     bool moving;
+#endif
+
 } L;
 
 
 static void move_camera(vec2 current_pos) {
     vec2 diff = vec2_sub_vec(current_pos, L.move0);
-    L.pos = vec2_sub_vec(L.pos, diff);
+    L.pos = vec2_sub_vec(L.pos0, diff);
     c_camera_set_pos(L.pos.x, L.pos.y);
 }
 
+#if GLES
 static void zoom_camera(float new_distance) {
     float factor = new_distance / L.distance0;
     L.size = L.size0 / factor;
     c_camera_set_size(L.size);
 }
+#endif
 
 static void wheel_event(bool up, void *user_data) {
     if (up)
@@ -49,8 +58,7 @@ void camera_control_pointer_event(ePointer_s pointer) {
         return;
 
     float alpha = 0.5;
-    L.touch[pointer.id].x = L.touch[pointer.id].x *(1-alpha) + pointer.pos.x * alpha;
-    L.touch[pointer.id].y = L.touch[pointer.id].y *(1-alpha) + pointer.pos.y * alpha;
+    L.touch[pointer.id] = vec2_mix(L.touch[pointer.id], pointer.pos.xy, alpha);
 
     if (pointer.action == E_POINTER_DOWN) {
         L.touching.v[pointer.id] = true;
@@ -66,21 +74,26 @@ void camera_control_pointer_event(ePointer_s pointer) {
         float distance = vec2_norm(vec2_sub_vec(L.touch[0], L.touch[1]));
 
         if (pointer.action == E_POINTER_DOWN) {
+            L.size0 = L.size;
+            L.pos0 = L.pos;
             L.move0 = mean;
             L.distance0 = distance;
-            L.size0 = L.size;
         } else {
-            //move_camera(mean);
+            move_camera(mean);
             zoom_camera(distance);
         }
     }
 #else
+    float alpha = 0.5;
+    L.pointer_pos = vec2_mix(L.pointer_pos, pointer.pos.xy, alpha);
     if(L.moving) {
-        move_camera(pointer.pos.xy);
+        move_camera(L.pointer_pos);
         if(pointer.action == E_POINTER_DOWN) {
             L.moving = false;
         }
     } else if(pointer.action == E_POINTER_MIDDLE_DOWN || pointer.action == E_POINTER_RIGHT_DOWN) {
+        L.pointer_pos = pointer.pos.xy;
+        L.pos0 = L.pos;
         L.move0 = pointer.pos.xy;
         L.moving = true;
     }
