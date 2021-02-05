140672b5ea733cea672a1d2cf1148e9a55548a8a
renehorstmann <r.horsty@gmail.com>
2021-02-05 10:37:48 +0100

    palette upgrade

--- src/palette.c
+++ src/palette.c
@@ -10,6 +10,7 @@
 #include "palette.h"
 
 #define COLOR_DROP_SIZE 16.0f
+#define MAX_ROWS 10
 
 static struct {
     Color_s palette[PALETTE_MAX];
@@ -44,26 +45,27 @@ static mat4 setup_palette_color_pose(int r, int c) {
     mat4 pose = mat4_eye();
     u_pose_set_size(&pose, COLOR_DROP_SIZE, COLOR_DROP_SIZE);
     if (hud_camera_is_portrait_mode()) {
-        u_pose_set_xy(&pose, hud_camera_left() + COLOR_DROP_SIZE/2 + c * COLOR_DROP_SIZE,
-                      hud_camera_bottom() + COLOR_DROP_SIZE/2 + r * COLOR_DROP_SIZE);
+        u_pose_set_xy(&pose, hud_camera_left() + COLOR_DROP_SIZE / 2 + c * COLOR_DROP_SIZE,
+                      hud_camera_bottom() + COLOR_DROP_SIZE / 2 + r * COLOR_DROP_SIZE);
     } else {
-        u_pose_set_xy(&pose, hud_camera_right() - COLOR_DROP_SIZE/2 - r * COLOR_DROP_SIZE,
-                      hud_camera_bottom() + COLOR_DROP_SIZE/2 + c * COLOR_DROP_SIZE);
+        u_pose_set_xy(&pose, hud_camera_right() - COLOR_DROP_SIZE / 2 - r * COLOR_DROP_SIZE,
+                      hud_camera_bottom() + COLOR_DROP_SIZE / 2 + c * COLOR_DROP_SIZE);
+        u_pose_set_angle(&pose, M_PI_2);
     }
     return pose;
 }
 
 void palette_init() {
-    GLuint drop_tex = r_texture_init_file("res/palette_color.png", NULL);
+    GLuint drop_tex = r_texture_init_file("res/palette_color_drop.png", NULL);
     r_texture_filter_nearest(drop_tex);
 
     r_ro_batch_init(&L.palette_ro, PALETTE_MAX, hud_camera_gl, drop_tex);
 
 
-    GLuint bg_tex = r_texture_init_file("res/palette_alpha_bg.png", NULL);
+    GLuint bg_tex = r_texture_init_file("res/palette_background.png", NULL);
     r_texture_filter_nearest(bg_tex);
 
-    r_ro_batch_init(&L.background_ro, PALETTE_MAX, hud_camera_gl, bg_tex);
+    r_ro_batch_init(&L.background_ro, PALETTE_MAX + MAX_ROWS, hud_camera_gl, bg_tex);
 
 
     GLuint select_tex = r_texture_init_file("res/palette_select.png", NULL);
@@ -112,13 +114,33 @@ void palette_update(float dtime) {
         // background uv
         {
             float u = i < L.palette_size ? 0 : 0.5;
-            u_pose_set(&L.background_ro.rects[i].uv, u, 0, 0.5, 1, 0);
+            float v = r < last_row ? 0.5 : 0;
+            u_pose_set(&L.background_ro.rects[i].uv, u, v, 0.5, 0.5, 0);
         }
     }
+
+    // background continuation
+    for (int r = 0; r < MAX_ROWS; r++) {
+        int idx = L.background_ro.num - r - 1;
+        // pose
+        mat4 pose = mat4_eye();
+        if (r <= last_row)
+            pose = setup_palette_color_pose(r, cols);
+        else
+            u_pose_set(&pose, FLT_MAX, FLT_MAX, 0, 0, 0);
+        L.background_ro.rects[idx].pose = pose;
+
+        // background uv
+        {
+            float v = r < last_row ? 0.5 : 0;
+            u_pose_set(&L.background_ro.rects[idx].uv, 0.5, v, 0.5, 0.5, 0);
+        }
+    }
+
     L.select_ro.rect.pose = L.palette_ro.rects[L.last_selected].pose;
 
-    r_ro_batch_update(&L.palette_ro, 0, PALETTE_MAX);
-    r_ro_batch_update(&L.background_ro, 0, PALETTE_MAX);
+    r_ro_batch_update(&L.palette_ro, 0, L.palette_ro.num);
+    r_ro_batch_update(&L.background_ro, 0, L.background_ro.num);
 }
 
 void palette_render() {
@@ -147,6 +169,7 @@ bool palette_pointer_event(ePointer_s pointer) {
 
 
 void palette_set_colors(const Color_s *palette, int size) {
+    assert(size < PALETTE_MAX);
     memcpy(L.palette, palette, sizeof(Color_s) * size);
     L.palette_size = size;
     palette_set_color(0);
