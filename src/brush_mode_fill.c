1790b5f0e6c4df91521cb8ce5b2aae7d1ee7b5e2
renehorstmann <r.horsty@gmail.com>
2021-02-09 22:28:57 +0100

    fill_uses_draw_pixel

--- src/brush_mode_fill.c
+++ src/brush_mode_fill.c
@@ -1,6 +1,6 @@
 #include "utilc/dynarray.h"
-#include "brush.h"
 #include "canvas.h"
+#include "brush.h"
 #include "brush_mode.h"
 
 
@@ -18,9 +18,12 @@ bool brush_mode_fill(ePointer_s pointer, bool mode8) {
     if(!image_contains_uv(img, uv))
         return false;
     
-    Color_s replace = *image_pixel_uv(img, layer, uv);
-    if(color_equals(brush_current_color, replace))
+    brush_secondary_color = *image_pixel_uv(img, layer, uv);
+    if(color_equals(brush_current_color, brush_secondary_color))
         return false;
+    
+    bool shading_was_active = brush_shading_active;
+    brush_shading_active = true;
     
     // PosStack needs to be killed
     PosStack stack = {0};
@@ -28,14 +31,10 @@ bool brush_mode_fill(ePointer_s pointer, bool mode8) {
     
     while(stack.size>0) 	{
     	ivec2 p = pos_stack_pop(&stack);
-    	if(!image_contains_uv(img, p))
+    	
+    	if(!brush_draw_pixel(p.x, p.y))
     	    continue;
-    	    
-    	Color_s *pixel = image_pixel_uv(img, layer, p);
-    	if(!color_equals(*pixel, replace))
-    	    continue;
-    	    
-    	*pixel = brush_current_color;
+    	
     	pos_stack_push(&stack, 
     	    (ivec2) {{p.x-1, p.y}});
     	pos_stack_push(&stack, 
@@ -59,6 +58,8 @@ bool brush_mode_fill(ePointer_s pointer, bool mode8) {
     }
     
     pos_stack_kill(&stack);
+    
+    brush_shading_active = shading_was_active;
     return true;
 }
 
